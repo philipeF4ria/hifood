495b1e580b193c8b17293af8341448174bbefa2f
Philipe Faria <philipefaria61@gmail.com>
2022-04-15 23:39:05 -0300

    AuthContext and login

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -2,8 +2,14 @@ import { AppProps } from 'next/app';
 
 import '../../styles/globals.scss';
 
+import { AuthProvider } from '../contexts/AuthContext';
+
 function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
+  );
 }
 
 export default App
--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { FormEvent, useContext, useState } from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
 import Link from 'next/link';
@@ -5,11 +6,32 @@ import Link from 'next/link';
 import { Input } from '../components/Input';
 import { Button } from '../components/Button';
 
+import { AuthContext } from '../contexts/AuthContext';
+
 import styles from '../../styles/home.module.scss';
 
 import logo from '../../public/logo.svg';
 
 export default function Home() {
+  const { signIn } = useContext(AuthContext);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  async function handleLogin(event: FormEvent) {
+    event.preventDefault();
+    
+    const data = {
+      email,
+      password,
+    };
+
+    signIn(data);
+
+    setEmail('');
+    setPassword('');
+  }
+
   return (
     <>
       <Head>
@@ -22,13 +44,18 @@ export default function Home() {
             <Input 
               placeholder="Seu E-mail"
               type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             />
             <Input 
               placeholder="Sua Senha"
               type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
             />
             <Button 
               loading={false}
+              onClick={handleLogin}
             >
               Fazer Login
             </Button>
